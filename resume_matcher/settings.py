35b5581f1b6f4f134de8290d8040ffb6a7e4c7fb
YamunaGit2002 <yamunamane13@gmail.com>
2025-06-07 19:52:29 +0530

    Update settings.py

--- resume_matcher/settings.py
+++ resume_matcher/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'resume_matcher',
         'USER': 'root',
-        'PASSWORD': 'security',
+        'PASSWORD': '********',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yamunamane13@gmail.com'
-EMAIL_HOST_PASSWORD = 'rzgjuhwpcughztfq '
+EMAIL_HOST_USER = 'xyz@gmail.com'
+EMAIL_HOST_PASSWORD = '**************** '
